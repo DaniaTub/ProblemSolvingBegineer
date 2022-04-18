ec39e2abc71a6cf0194f0656208602ddefe9b046
Dania <daniatub8@gmail.com>
2022-04-18 20:50:24 +0300

    SquareRoot

---
+++ src/com/Dania/question/six.java
@@ -0,0 +1,15 @@
+package com.Dania.question;
+//SquareRoot
+
+import java.math.*;
+import java.util.Scanner;
+
+public class six {
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        double x=  scan.nextDouble();
+        int a=(int)Math.sqrt(x);
+        System.out.println(a);
+
+    }
+}
